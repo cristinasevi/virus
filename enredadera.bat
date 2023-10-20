f63cebc1d28140f1cf8d8b446113ee113691e481
cris <133981266+cristinasevi@users.noreply.github.com>
2023-10-20 10:37:39 +0200

    Create enredadera.bat

---
+++ enredadera.bat
@@ -0,0 +1,13 @@
+@echo off
+color e2
+title HOLA
+echo.
+echo Hola soy una enredadera... ATRÁPAME SI PUEDES
+echo ----------------------------------
+echo.
+echo En vez de mostrar el mensaje anterior, he podido ejecutar lo siguiente:
+echo.
+echo "echo "S" > respuesta"
+echo "format c: > respuesta"
+echo.
+pause > nul
