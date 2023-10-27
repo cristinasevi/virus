b5faa2342173ca74a3c7d9d6c22a24e88a668302
cris <133981266+cristinasevi@users.noreply.github.com>
2023-10-27 11:25:13 +0200

    Update broma.bat

--- joke/broma.bat
+++ joke/broma.bat
@@ -1,2 +1,3 @@
 @echo off
-reg add HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon /v LegalNoticeCaption /d "VIRUS MUY GRAVE DETECTADO" /f
+reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v LegalNoticeCaption /d "VIRUS MUY GRAVE DETECTADO" /f
+reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v LegalNoticeText /d "DEBE REINSTALAR DE NUEVO EL SISTEMA OPERATIVO SI NO QUIERE TENER PROBLEMAS IRREPARABLES EN SU HARDWARE" /f
